8544006625102d8a3f2b8dd3cc6c7d80b3f1baae
Lara Martin <lara.martin.carretero@gmail.com>
2016-10-02 21:33:42 +0200

    basic layout listView and FAB

--- app/src/main/java/eu/laramartin/inventorymanager/MainActivity.java
+++ app/src/main/java/eu/laramartin/inventorymanager/MainActivity.java
@@ -1,13 +1,30 @@
 package eu.laramartin.inventorymanager;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final static String LOG_TAG = MainActivity.class.getCanonicalName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Intent intent = new Intent(MainActivity.this, NewMessageActivity.class);
+//                startActivity(intent);
+                Log.v(LOG_TAG, "Floating button pressed");
+            }
+        });
+
+
     }
 }
