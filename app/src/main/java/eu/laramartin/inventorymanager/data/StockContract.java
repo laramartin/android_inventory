f336f6025b6d7c1d1b2553b6ffbcb6e49d46ad23
Lara Martin <lara.martin.carretero@gmail.com>
2016-10-03 10:17:28 +0200

    corrected naming style and typo

--- app/src/main/java/eu/laramartin/inventorymanager/data/StockContract.java
+++ app/src/main/java/eu/laramartin/inventorymanager/data/StockContract.java
@@ -18,18 +18,19 @@ public class StockContract {
         public static final String COLUMN_NAME = "name";
         public static final String COLUMN_PRICE = "price";
         public static final String COLUMN_QUANTITY = "quantity";
-        public static final String COLUMN_SUPPLIER_NAME = "supplierName";
-        public static final String COLUMN_SUPPLIER_PHONE = "supplierPhone";
-        public static final String COLUMN_SUPPLIER_EMAIL = "supplierEmail";
+        public static final String COLUMN_SUPPLIER_NAME = "supplier_name";
+        public static final String COLUMN_SUPPLIER_PHONE = "supplier_phone";
+        public static final String COLUMN_SUPPLIER_EMAIL = "supplier_email";
 
-        public static final String CREATE_TABLE_STOCK = "CREATE TABLE " + StockContract.StockEntry.TABLE_NAME + "(" +
+        public static final String CREATE_TABLE_STOCK = "CREATE TABLE " +
+                StockContract.StockEntry.TABLE_NAME + "(" +
                 StockContract.StockEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
                 StockContract.StockEntry.COLUMN_NAME + " TEXT NOT NULL," +
                 StockContract.StockEntry.COLUMN_PRICE + " TEXT NOT NULL," +
                 StockContract.StockEntry.COLUMN_QUANTITY + " INTEGER NOT NULL DEFAULT 0," +
                 StockContract.StockEntry.COLUMN_SUPPLIER_NAME + " TEXT NOT NULL," +
                 StockContract.StockEntry.COLUMN_SUPPLIER_PHONE + " TEXT NOT NULL," +
-                StockContract.StockEntry.COLUMN_SUPPLIER_EMAIL + " TEXT NOT NULL" + ")";
+                StockContract.StockEntry.COLUMN_SUPPLIER_EMAIL + " TEXT NOT NULL" + ");";
 
 //        public static final String SUPPLIER_NAME_DRINKS = "Durinku JP";
 //        public static final String SUPPLIER_PHONE_DRINKS = "+81 123-456-789";
