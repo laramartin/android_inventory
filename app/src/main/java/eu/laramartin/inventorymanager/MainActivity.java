a740f73fcadbf25d76d926453544b85ab81c4039
Lara Martin <lara.martin.carretero@gmail.com>
2016-10-06 20:56:13 +0200

    hide/show fab when scrolling 1 whole item

--- app/src/main/java/eu/laramartin/inventorymanager/MainActivity.java
+++ app/src/main/java/eu/laramartin/inventorymanager/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
@@ -17,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     private final static String LOG_TAG = MainActivity.class.getCanonicalName();
     InventoryDbHelper dbHelper;
     StockCursorAdapter adapter;
+    int lastVisibleItem = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         dbHelper = new InventoryDbHelper(this);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -35,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        ListView listView = (ListView) findViewById(R.id.list_view);
+        final ListView listView = (ListView) findViewById(R.id.list_view);
         View emptyView = findViewById(R.id.empty_view);
         listView.setEmptyView(emptyView);
 
@@ -50,6 +52,27 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(MainActivity.this, DetailsActivity.class);
                 intent.putExtra("itemId", itemId);
                 startActivity(intent);
+            }
+        });
+        listView.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+                if(scrollState == 0) return;
+                    final int currentFirstVisibleItem = view.getFirstVisiblePosition();
+                    if (currentFirstVisibleItem > lastVisibleItem) {
+                        fab.show();
+                        Log.i("a", "scrolling down...");
+                    } else if (currentFirstVisibleItem < lastVisibleItem) {
+                        fab.hide();
+                        Log.i("a", "scrolling up...");
+                    }
+
+                lastVisibleItem = currentFirstVisibleItem;
+            }
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+
             }
         });
     }
